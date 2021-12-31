2eb84eaa8ad1b314dcd5fd0e09088742c05941e6
Laura Cole <lauracole1900@comcast.net>
2021-12-31 03:57:45 -0700

    concert dates format correctly & added some styling to eventCards

--- client/src/components/cards/concertCard.js
+++ client/src/components/cards/concertCard.js
@@ -1,10 +1,18 @@
 // import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
-// import dayjs from "dayjs";
+import dayjs from "dayjs";
 import "./style.css";
 
 const ConcertCard = ({ concert }) => {
-  const dates = concert.date.length > 1 ? concert.date.join(" & ") : concert.date[0].toString();
+  const formatDate = (dates) => {
+    for (const date of dates) {
+      const formattedDate = dayjs(date, "MM-DD-YYYY").format("dddd, MMM D, YYYY");
+      return formattedDate;
+    }
+    return dates.length > 1 ? dates.join(" & ") : dates[0].toString();
+  }
+
+  const dates = formatDate(concert.date);
   const times = concert.time.length > 1 ? concert.time.join(" & ") : concert.time[0].toString();
   const venues = concert.venue.length > 1 ? concert.venue.join(" & ") : concert.venue[0].toString();
   console.log(concert.time, times);
@@ -17,14 +25,14 @@ const ConcertCard = ({ concert }) => {
         </Card.Header>
         <Card.Body className="concertBody">
           {dates.includes("&")
-            ? <p>Dates: {dates}</p>
-            : <p>Date: {dates}</p>}
+            ? <p><span className="bold">Dates:</span> {dates}</p>
+            : <p><span className="bold">Date:</span> {dates}</p>}
           {times.includes("&")
-            ? <p>Times: {times}</p>
-            : <p>Time: {times}</p>}
+            ? <p><span className="bold">Times:</span> {times}</p>
+            : <p><span className="bold">Time:</span> {times}</p>}
           {venues.includes("&")
-            ? <p>Locations: {venues}</p>
-            : <p>Location: {venues}</p>}
+            ? <p><span className="bold">Locations:</span> {venues}</p>
+            : <p><span className="bold">Location:</span> {venues}</p>}
         </Card.Body>
       </Card>
     </>
