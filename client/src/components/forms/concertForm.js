d4283e9e1365461b93de1d821b77c91cd2c61689
Laura Cole <lauracole1900@comcast.net>
2022-01-03 22:13:21 -0700

    imported concertFormValidation into concertForm & added error-handling to JSX

--- client/src/components/forms/concertForm.js
+++ client/src/components/forms/concertForm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Form, Row } from "./react-bootstrap";
+import { concertValidate } from "../../utils/validation";
 import "./style.css";
 
 const ConcertForm = () => {
@@ -10,6 +11,7 @@ const ConcertForm = () => {
     venue: [],
     addlMaterials: [],
   });
+  const [errors, setErrors] = useState({});
 
   // Handles input changes to form fields
   const handleInputChange = (e) => {
@@ -17,10 +19,10 @@ const ConcertForm = () => {
     setConcertData({ ...concertData, [name]: value });
     if (["date", "time", "venue", "addlMaterials"].includes(name)) {
       let data = value.split(",").trim();
-      setConcertData({...concertData, [name]: data});
+      setConcertData({ ...concertData, [name]: data });
     }
   };
-  
+
 
   return (
     <>
@@ -36,12 +38,16 @@ const ConcertForm = () => {
             <Form.Group controlId="formConcertName">
               <Col sm={6}>
                 <Form.Label>Name of concert or event: <span className="red">*</span></Form.Label>
+                {errors.name &&
+                  <div className="error"><p>{errors.name}</p></div>}
                 <Form.Control type="input" name="name" placeholder="Our Fantastic Concert" value={concertData.name} className="formInput" onChange={handleInputChange} />
               </Col>
             </Form.Group>
             <Form.Group controlId="formConcertDate">
               <Col sm={6}>
                 <Form.Label>Date(s) of concert or event: <span className="red">*</span></Form.Label>
+                {errors.date &&
+                  <div className="error"><p>{errors.date}</p></div>}
                 <Form.Text className="subtitle" muted>Please enter dates in the format MM-DD-YYYY and, for events that occur on multiple dates (auditions, tours, etc.), separate dates with commas</Form.Text>
                 <Form.Control type="input" name="date" placeholder="04-05-2063" value={concertData.date} className="formInput" onChange={handleInputChange} />
               </Col>
@@ -51,6 +57,8 @@ const ConcertForm = () => {
             <Form.Group controlId="formConcertTime">
               <Col sm={6}>
                 <Form.Label>Time(s) of concert or event: <span className="red">*</span></Form.Label>
+                {errors.time &&
+                  <div className="error"><p>{errors.time}</p></div>}
                 <Form.Text className="subtitle" muted>Please enter times in the format HH:MMam and, for events that occur at multiple times (Pops & Pasta, etc.), separate times with commas</Form.Text>
                 <Form.Control type="input" name="time" placeholder="7:30pm" value={concertData.date} className="formInput" onChange={handleInputChange} />
               </Col>
@@ -58,6 +66,8 @@ const ConcertForm = () => {
             <Form.Group controlId="formConcertVenue">
               <Col sm={6}>
                 <Form.Label>Venue(s) of concert or event: <span className="red">*</span></Form.Label>
+                {errors.venue &&
+                  <div className="error"><p>{errors.venue}</p></div>}
                 <Form.Text className="subtitle" muted>For events that occur at multiple venues (tours, etc.), please separate venues with commas</Form.Text>
                 <Form.Control type="input" name="venue" placeholder="Frasier 63" value={concertData.venue} className="formInput" onChange={handleInputChange} />
               </Col>
