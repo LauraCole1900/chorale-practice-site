6d4288339c919d1344b23bd931267d3ee193deea
Laura Cole <lauracole1900@comcast.net>
2022-01-15 18:08:40 -0700

    coded confirmModal, untested

--- client/src/components/modals/confirmModal.js
+++ client/src/components/modals/confirmModal.js
@@ -1,3 +1,4 @@
+import { Button, Modal } from "react-bootstrap";
 import "./style.css";
 
 const ConfirmModal = (props) => {
@@ -5,7 +6,55 @@ const ConfirmModal = (props) => {
 
   return (
     <>
+      <Modal show={props.show} onHide={props.hide} backdrop="static" keyboard={false} centered={true} className="modal">
+        <Modal.Header className="modalHeadConf">
+          <Modal.Title className="modalTitle"><h2>Please confirm</h2></Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modalBody">
+          <h3>Are you sure?</h3>
 
+          {/* Delete Event button*/}
+          {props.btnname === "concertDelete" &&
+            <h4>Are you sure you want to delete {props.concert.name}? This action can't be undone.</h4>}
+
+          {/* Delete Member button*/}
+          {props.btnname === "memberDelete" &&
+            <h4>Are you sure you want to delete {props.user.fullName}? This action can't be undone.</h4>}
+
+          {/* Delete Post button */}
+          {props.btnname === "postDelete" &&
+            <h4>Are you sure you want to delete {props.post.title} from {props.post.date}? This action can't be undone.</h4>}
+
+          {/* Delete Repertoire button*/}
+          {props.btnname === "songsDelete" &&
+            <h4>Are you sure you want to delete the selected repertoire? This action can't be undone.</h4>}
+
+          {/* Navigation buttons */}
+          <Modal.Footer className="modalFooter">
+
+            {/* Confirm Cancel Conference button */}
+            {props.btnname === "concertDelete" &&
+              <Button data-toggle="popover" title="Confirm Delete" className="button" type="button" onClick={props.eventDelete}>Yes, Cancel {props.concert.name}</Button>}
+
+            {/* Confirm Delete Session button */}
+            {props.btnname === "memberDelete" &&
+              <Button data-toggle="popover" title="Confirm Delete" className="button" type="button" onClick={props.memberDelete}>Yes, Delete {props.user.fullName}</Button>}
+
+            {/* Confirm Deactivate Presenter button */}
+            {props.btnname === "postDelete" &&
+              <Button data-toggle="popover" title="Confirm Delete" className="button" type="button" onClick={props.postDelete}>Yes, Delete {props.post.title} from {props.post.date}</Button>}
+
+            {/* Confirm Unregister button */}
+            {props.btnname === "songsDelete" &&
+              <Button data-toggle="popover" title="Confirm Delete" className="button" type="button" onClick={props.songsDelete}>Yes, Delete Repertoire</Button>}
+
+            {/* No, take no action button */}
+            <Button data-toggle="popover" title="No" className="button" type="button" onClick={props.hide}>No, take me back</Button>
+
+          </Modal.Footer>
+
+        </Modal.Body>
+      </Modal>
     </>
   )
 }
