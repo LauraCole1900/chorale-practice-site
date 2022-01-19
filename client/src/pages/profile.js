d46c27b2c32ca538e429d5e07899f8a49cef3706
Laura Cole <lauracole1900@comcast.net>
2022-01-19 08:59:11 -0700

    added conditional redirects to profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -30,41 +30,46 @@ const ProfilePage = () => {
     console.log(JSON.stringify(meError));
   };
 
+  if (!Auth.loggedIn()) {
+    <Navigate to="/login" />
+  };
+
+  if (me.position === "guest") {
+    <Navigate to="/members" />
+  }
+
 
   return (
     <>
-      {!Auth.loggedIn()
-        ? <Navigate to="/login" />
-        : <Container>
-          <Row>
-            <Col sm={2}>
-              <Sidenav user={me} />
-            </Col>
-            <Col sm={8}>
-              <Card className="profileCard">
-                <Card.Header className="cardTitle">
-                  <h1>My Profile</h1>
-                </Card.Header>
-                <Card.Body className="cardBody">
-                  <p>Full name: {me.fullName}</p>
-                  <p>Prefer to be called: {me.preferredName}</p>
-                  <p>Section: {me.section}</p>
-                  <p>Birthday: {formatDate(me.birthday)}</p>
-                  <p>Primary email: {me.email1}</p>
-                  <p>Secondary email: {me.email2}</p>
-                  <p>Primary phone: {me.phone1} ({me.phone1Type})</p>
-                  <p>Secondary phone: {me.phone2} ({me.phone2Type})</p>
-                  <p>Tertiary phone: {me.phone3} ({me.phone3Type})</p>
-                  <p>Street address: {me.streetAddress}</p>
-                  <p>City: {me.city}</p>
-                  <p>State: {me.state}</p>
-                  <p>ZIP Code: {me.zipCode}</p>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      }
+      <Container>
+        <Row>
+          <Col sm={2}>
+            <Sidenav user={me} />
+          </Col>
+          <Col sm={8}>
+            <Card className="profileCard">
+              <Card.Header className="cardTitle">
+                <h1>My Profile</h1>
+              </Card.Header>
+              <Card.Body className="cardBody">
+                <p>Full name: {me.fullName}</p>
+                <p>Prefer to be called: {me.preferredName}</p>
+                <p>Section: {me.section}</p>
+                <p>Birthday: {formatDate(me.birthday)}</p>
+                <p>Primary email: {me.email1}</p>
+                <p>Secondary email: {me.email2}</p>
+                <p>Primary phone: {me.phone1} ({me.phone1Type})</p>
+                <p>Secondary phone: {me.phone2} ({me.phone2Type})</p>
+                <p>Tertiary phone: {me.phone3} ({me.phone3Type})</p>
+                <p>Street address: {me.streetAddress}</p>
+                <p>City: {me.city}</p>
+                <p>State: {me.state}</p>
+                <p>ZIP Code: {me.zipCode}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
