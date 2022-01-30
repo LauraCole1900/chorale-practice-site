e9df5b9d11d29e7d75dda88661003a291581de1a
Laura Cole <lauracole1900@comcast.net>
2022-01-29 17:21:22 -0700

    editRepertoire now returns data to update the cache

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -140,16 +140,15 @@ const resolvers = {
     },
 
     editRepertoire: async (_, args) => {
-      console.log({ args });
       const concert = await Concert.findOneAndUpdate({ _id: args._id, "songs._id": args.songId }, {
         $set: {
           "songs.$": {
-            title: args.title, composer: args.composer, concertOrder: args.concertOrder, practiceTrackUrlsSopSlow: args.practiceTrackUrlsSopSlow, practiceTrackUrlsAltoSlow: args.practiceTrackUrlsAltoSlow, practiceTrackUrlsTenSlow: args.practiceTrackUrlsTenSlow, practiceTrackUrlsBassSlow: args.practiceTrackUrlsBassSlow, practiceTrackUrlsSopATempo: args.practiceTrackUrlsSopATempo, practiceTrackUrlsAltoATempo: args.practiceTrackUrlsAltoATempo, practiceTrackUrlsTenATempo: args.practiceTrackUrlsTenATempo, practiceTrackUrlsBassATempo: args.practiceTrackUrlsBassATempo, videoUrls: args.videoUrls
+            _id: args.songId, title: args.title, composer: args.composer, concertOrder: args.concertOrder, practiceTrackUrlsSopSlow: args.practiceTrackUrlsSopSlow, practiceTrackUrlsAltoSlow: args.practiceTrackUrlsAltoSlow, practiceTrackUrlsTenSlow: args.practiceTrackUrlsTenSlow, practiceTrackUrlsBassSlow: args.practiceTrackUrlsBassSlow, practiceTrackUrlsSopATempo: args.practiceTrackUrlsSopATempo, practiceTrackUrlsAltoATempo: args.practiceTrackUrlsAltoATempo, practiceTrackUrlsTenATempo: args.practiceTrackUrlsTenATempo, practiceTrackUrlsBassATempo: args.practiceTrackUrlsBassATempo, videoUrls: args.videoUrls
           }
         }
       }, { new: true });
-      console.log({ concert });
-      return concert;
+      const song = concert.songs.filter(song => song._id == args.songId);
+      return song[0];
     },
 
     editPassword: async (_, args) => {
