3dbae946f19afb8c4edfeb4b72d891fce195339a
Laura Cole <lauracole1900@comcast.net>
2022-02-17 20:44:03 -0700

    small adjustments to audioEmbed and videoEmbed; audioEmbed doesn't work yet

--- client/src/components/embed/audioEmbed.js
+++ client/src/components/embed/audioEmbed.js
@@ -1,18 +1,18 @@
 import PropTypes from "prop-types";
 
-const AudioEmbed = ({ src }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={src}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+const AudioEmbed = ({ title, src }) => {
+  console.log({ title }, { src });
+
+  return (
+    <div className="audio-responsive" >
+      <audio
+        src={src}
+        title={title}
+        controls
+      />
+    </div>
+  )
+};
 
 AudioEmbed.propTypes = {
   src: PropTypes.string.isRequired
--- client/src/components/embed/videoEmbed.js
+++ client/src/components/embed/videoEmbed.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 
-const VideoEmbed = ({ src }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={src}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+const VideoEmbed = ({ src }) => {
+
+
+  return (
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={src}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  )
+};
 
 VideoEmbed.propTypes = {
   src: PropTypes.string.isRequired
