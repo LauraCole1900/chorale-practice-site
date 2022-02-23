6ca8ffed15bdd4f48aa30f8f02f21b08c0488b0e
Laura Cole <lauracole1900@comcast.net>
2022-02-22 21:12:13 -0700

    fixed what collapses on topnav

--- client/src/components/navbar/topnav.js
+++ client/src/components/navbar/topnav.js
@@ -30,37 +30,37 @@ const Navibar = () => {
   return (
     <>
       <Navbar expand="sm" className="navbar">
-        <Navbar.Brand>
-          <div>
-            {/* <Image src={gclogo} alt="Greeley Chorale logo" /> */}
-            <Link to="/" className="navlink" aria-label="Greeley Chorale Logo">
-              The Greeley Chorale Practice Site
-            </Link>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav" className="navbarCollapse">
-          <Nav>
-            <Link to="/" className="navlink">
-              Home
-            </Link>
-            {Auth.loggedIn() ? (
-              <>
-                {me.position !== "guest" &&
-                  <Link to="/profile" className="navlink">
-                    Profile
-                  </Link>}
-                <Link to="/members" className="navlink">
-                  Members
-                </Link>
-                <Nav.Link onClick={Auth.logout} className="navlink">Logout</Nav.Link>
-              </>
-            ) : (
-              <Link to="/login" className="navlink">
-                Login
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Brand>
+            <div>
+              {/* <Image src={gclogo} alt="Greeley Chorale logo" /> */}
+              <Link to="/" className="navlink" aria-label="Greeley Chorale Logo">
+                The Greeley Chorale Practice Site
               </Link>
-            )}
-          </Nav>
+            </div>
+          </Navbar.Brand>
         </Navbar.Collapse>
+        <Nav className="navbarKeep">
+          <Link to="/" className="navlink">
+            Home
+          </Link>
+          {Auth.loggedIn() ? (
+            <>
+              {me.position !== "guest" &&
+                <Link to="/profile" className="navlink">
+                  Profile
+                </Link>}
+              <Link to="/members" className="navlink">
+                Members
+              </Link>
+              <Nav.Link onClick={Auth.logout} className="navlink">Logout</Nav.Link>
+            </>
+          ) : (
+            <Link to="/login" className="navlink">
+              Login
+            </Link>
+          )}
+        </Nav>
       </Navbar>
     </>
   )
