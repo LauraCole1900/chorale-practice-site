d00cce5a8c9727a3dbfc4d8eeacda0bfda72833c
Laura Cole <lauracole1900@comcast.net>
2022-08-26 21:02:17 -0600

    practice track urls are in array in tracksAccordion

--- client/src/components/cards/tracksAccordion.js
+++ client/src/components/cards/tracksAccordion.js
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
-import { Accordion } from "react-bootstrap";
+import { Accordion, Button } from "react-bootstrap";
 import dayjs from "dayjs";
 import { timeToCurtain } from "../../utils/dateUtils";
 import SongCard from "./songCard";
 import "./style.css";
 
 
-const TracksAccordion = ({ concert, i, section }) => {
+const TracksAccordion = ({ concert, i, section, user }) => {
+
+  let truncSect = "";
+  if (section === "soprano" || section === "tenor") {
+    const truncated = section.slice(0, 3);
+    truncSect = `${truncated.charAt(0).toUpperCase()}${truncated.slice(1)}`;
+  } else if (section === "alto" || section === "bass") {
+    truncSect = `${section.charAt(0).toUpperCase()}${section.slice(1)}`;
+  };
+
 
   //=====================//
   //   State variables   //
   //=====================//
   const [allSongs, setAllSongs] = useState(concert.songs);
+  const [playlist, setPlaylist] = useState([]);
 
 
   //=====================//
@@ -23,6 +33,24 @@ const TracksAccordion = ({ concert, i, section }) => {
     const formattedDate = dates.map(date => dayjs(date, "M-D-YYYY").format("dddd, MMM D, YYYY"));
     return formattedDate.length > 1 ? formattedDate.join(" & ") : formattedDate[0].toString();
   }
+
+  // Handles click on "play full playlist" button
+  const handlePlaylistPlay = (e) => {
+    const { value } = e.target;
+    console.log({ playlist });
+  };
+
+  // Handles click on "pause full playlist" button
+  const handlePlaylistPause = (e) => {
+    const { value } = e.target;
+    JSON.parse(value) ? setPlaylist(false) : setPlaylist(true);
+  };
+
+  // Handles click on "stop full playlist" button
+  const handlePlaylistStop = (e) => {
+    const { value } = e.target;
+    JSON.parse(value) ? setPlaylist(false) : setPlaylist(true);
+  };
 
 
   //=====================//
@@ -48,9 +76,14 @@ const TracksAccordion = ({ concert, i, section }) => {
     if (filteredSongs.length) {
       const sortedSongs = filteredSongs.sort((a, b) => a.concertOrder > b.concertOrder ? 1 : -1);
       setAllSongs([...sortedSongs, otherSongs].flat());
+      const trackUrls = sortedSongs.map(song => song[`practiceTrackUrls${truncSect}`]);
+      const flattenedUrls = trackUrls.flat();
+      const filteredUrls = flattenedUrls.filter(url => !url.includes("cyberbass"));
+      setPlaylist(filteredUrls);
     }
 
-  }, [concert.songs]);
+
+  }, [concert.songs, section]);
 
 
   return (
@@ -61,6 +94,11 @@ const TracksAccordion = ({ concert, i, section }) => {
           <p>{dates} | {times} | <span className="boldOutline">Countdown to curtain</span>: {timeToCurtain(concert.date[0], concert.time[0])}</p>
         </Accordion.Header>
         <Accordion.Body className="cardBody">
+          {allSongs.length > 0 &&
+            <>
+              
+            </>
+          }
           {concert.addlMaterials?.length > 0 &&
             concert.addlMaterials.map((item, i) => (
               <h4 key={i}>Supplemental materials: <a href={item} target="_blank" rel="noreferrer noopener">Supplement {i + 1}</a></h4>
